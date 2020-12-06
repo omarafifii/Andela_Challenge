c3849eecd5f844a83f948d7b5f110dec698d8646
omarafifii <omarafifi@windowslive.com>
2020-12-06 16:36:26 +0200

    Updated the files

--- andela_challenge_final.py
+++ andela_challenge_final.py
@@ -6,14 +6,8 @@ import json
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 
-api_secret = '4vrqxr9466bkne567kf2kgxf'
-zip_code = '78701'
-start_date = datetime.datetime.now().strftime('%Y-%m-%d')
-line_up_id = 'USA-TX42500-X'
-date_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M')
-
 engine = sqlalchemy.create_engine(
-    'mysql+mysqlconnector://omar:omar@localhost:3306/sqlalchemy',
+    'mysql+mysqlconnector://omar:******@localhost:3306/sqlalchemy',
     echo=True)
 
 Base = declarative_base()
@@ -43,21 +37,24 @@ Base.metadata.create_all(engine)
 
 def get_data_from_api(api_secret,zip_code,start_date,line_up_id,date_time):
     
+    # Get data from first link
     r1 = requests.get('http://data.tmsapi.com/v1.1/movies/showings?startDate={}&zip={}&api_key={}'.format(start_date,zip_code,api_secret))
 
+    #  Some data wrangling to obtain the shape we need
     r1_json = json.loads(r1.text)
     r1_df = pd.json_normalize(r1_json, record_path=['showtimes'] ,meta=['title','shortDescription','genres','releaseYear'], errors='ignore')
     r1_df_final = r1_df[['theatre.name','title', 'releaseYear','genres', 'shortDescription','dateTime']]
     r1_df_final.columns = ['theatre','title', 'releaseYear','genres', 'shortDescription','dateTime']
     r1_df_final.dropna(inplace=True)
-    # r1_df_final.genres = r1_df_final['genres'].fillna({i: [] for i in r1_df_final.index})
     r1_df_final['genres'] = r1_df_final['genres'].apply(','.join)
 
+    # Store to database
     r1_df_final.to_sql("theatres", con=engine, index=False, if_exists="replace")
 
-    
+    # Get data from second link
     r2 = requests.get('http://data.tmsapi.com/v1.1/movies/airings?lineupId={}&startDateTime={}Z&api_key={}'.format(line_up_id,date_time,api_secret))
 
+    #  Some data wrangling to obtain the shape we need
     r2_json = json.loads(r2.text)
     r2_df = pd.json_normalize(r2_json)
     r2_df_final = r2_df[['channels','program.title', 'program.releaseYear','program.genres', 'program.shortDescription']]
@@ -66,21 +63,32 @@ def get_data_from_api(api_secret,zip_code,start_date,line_up_id,date_time):
     r2_df_final['genres'] = r2_df_final['genres'].apply(','.join)
     r2_df_final['channel'] = r2_df_final['channel'].apply(','.join)
 
+    # Store to database
     r2_df_final.to_sql("channels", con=engine, index=False, if_exists="replace")
 
 def get_top_five_genres():
+    # Get data from database
     r1_new = pd.read_sql_table('theatres', con=engine)
     r2_new = pd.read_sql_table('channels', con=engine)
+
+    # Merge data into one table
     df_total = pd.concat([r1_new, r2_new])
+
+    # Process the data to get the top five genres and return their movie details
     df_total['genres'] = df_total['genres'].apply(lambda x: x.split(','))
     df_total = df_total.explode('genres')
     top_5 = df_total.groupby('genres')['title'].count().sort_values(ascending=False)[:5]
     top_5_list = list(top_5.index.values)
-    # print('The top 5 genres are:\n')
-    # print(top_5)
     df_top_5_final = df_total[df_total['genres'].isin(top_5_list)]
+    df_top_5_final = df_top_5_final.reset_index(drop=True)
     return top_5, df_top_5_final
 
+
+api_secret = '*************'
+zip_code = '78701'
+start_date = datetime.datetime.now().strftime('%Y-%m-%d')
+line_up_id = 'USA-TX42500-X'
+date_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M')
 
 get_data_from_api(api_secret,zip_code,start_date,line_up_id,date_time)
 result = get_top_five_genres()
